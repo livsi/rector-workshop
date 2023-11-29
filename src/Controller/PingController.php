509a61f8741b65621adbafbd39e08b78e45e1c05
LiVsI <v.lezhnev@gmail.com>
2023-11-29 15:58:53 +0000

    add rector

--- src/Controller/PingController.php
+++ src/Controller/PingController.php
@@ -8,11 +8,25 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PingController extends Controller
 {
+    const RETURN_MESSAGE="You number %s";
     /**
      * @Route("/ping", name="ping")
      */
     public function pingAction(): JsonResponse
     {
         return $this->json(["message"=>"pong"]);
+    }
+
+    /**
+     * @Route("/return", name="return", requirements="\d+")
+     */
+    public function returnAction($number): JsonResponse
+    {
+        return $this->json(["message"=>sprintf(self::RETURN_MESSAGE, $number)]);
+    }
+
+    private function dead()
+    {
+        return "Function dead";
     }
 }
