d7aa83b92b5dc294e4e81a29fcfd710b1a5ea592
LiVsI <v.lezhnev@gmail.com>
2023-11-29 12:13:28 +0000

    downgrade dev deps, add compose configs

--- src/Controller/PingController.php
+++ src/Controller/PingController.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PingController extends Controller
 {
     /**
      * @Route("/ping", name="ping")
      */
-    public function index(): JsonResponse
+    public function pingAction(): JsonResponse
     {
         return $this->json(["message"=>"pong"]);
     }
